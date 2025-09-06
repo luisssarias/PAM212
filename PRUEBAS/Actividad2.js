19ed334d0a8839916407b71cb32d3a921c1936be
luisssarias <124051525@upq.edu.mx>
2025-09-05 22:19:20 -0600

    Completamos la información de los botones

--- PRUEBAS/Actividad2.js
+++ PRUEBAS/Actividad2.js
@@ -6,17 +6,19 @@ class InicioPAM {
     }
 
     desplegarLineamientosClassroom() {
-       const contenedor = document.getElementById("Contenido");
-       contenedor.innerHTML = "<h1>Lineamientos Classroom</h1><p>Contenido de los lineamientos...</p>";
+        const contenedor = document.getElementById("Contenido");
+        contenedor.innerHTML = "<h1>Lineamientos</h1><ol><li>Todos los trabajos llevan portada estilo libre: Logo UPQ, tema, datos de alumno, materia</li><li>Conclusiones de aprendizaje: Descripción de lo aprendido durante el desarrollo de la actividad </li></ol>";
     }
 
-    desplegarFechasParciales() {
-       const contenedor = document.getElementById("Contenido");
-       contenedor.innerHTML = "<h1>Fechas Parciales</h1><p>Contenido de las fechas parciales...</p>";
+    desplegarFechasParciales(){
+        const contenedor = document.getElementById("Contenido");
+        contenedor.innerHTML = " <h1>Fechas de Exámenes Parciales</h1><ul><li>Parcial 1: 29/09/2025</li><li>Parcial 2: 03/10/2025</li><li>Parcial 3: 01/12/2025</li></ul>";
     }
 
     desplegarPorcentajesPorParcial() {
-       const contenedor = document.getElementById("Contenido");
-       contenedor.innerHTML = "<h1>Porcentajes por Parcial</h1><p>Contenido de los porcentajes por parcial...</p>";
-    }
+        const contenedor = document.getElementById("Contenido");
+        contenedor.innerHTML = "<h1>Porcentajes</h1><ul><li>Parcial 1<ol><li>Conocimiento: 40%</li><li>Desempeño: 20%</li><li>Producto: 30%</li><li>PI: 10%</li></ol></li><li>Parcial 2<ol><li>Conocimiento: 40%</li><li>Desempeño: 20%</li><li>Producto: 20%</li><li>PI: 20%</li></ol></li><li>Parcial 3<ol><li>Conocimiento: 20%</li><li>Desempeño: 10%</li><li>Producto: 40%</li><li>PI: 30%</li></ol></li></ul>";
+    }
+
 }
+    
