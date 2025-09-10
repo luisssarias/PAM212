ba42e8baf52ad7eaab139bf75d58e2869f2c782c
luisssarias <124051525@upq.edu.mx>
2025-09-09 18:25:36 -0600

    Tipos de datos

---
+++ JSPrincipiantes/TiposDeDatos.js
@@ -0,0 +1,36 @@
+// Tipos de datos:
+"Hello World" // STRING
+'Hello World' // STRING
+
+
+1000000 // NUMBER
+1000.2 // NUMBER
+
+true // BOOLEAN
+false // BOOLEAN
+
+//ARRAYS
+
+["Joe", "Ryan", "Marta"] // ARRAY DE STRINGS
+[1, 2, 3, 4, 5] // ARRAY DE NÚMEROS
+[true, false, true] // ARRAY DE BOOLEANOS
+
+//OBJETOS
+"Ryan"
+70.4
+14
+//EJEMPLO
+true
+{
+    "username": "Ryan",
+    "score": 70.4,
+    "hours": 14,
+    "professional": true
+}
+
+{
+    "username": "Joe",
+    "score": 0.4,
+    "hours": 1,
+    "professional": false
+}
