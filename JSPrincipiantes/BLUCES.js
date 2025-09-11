b570b10ebf9977cd6ee3fc5f4f4f05700a034fb3
luisssarias <124051525@upq.edu.mx>
2025-09-11 13:12:38 -0600

    BUCLES

---
+++ JSPrincipiantes/BLUCES.js
@@ -0,0 +1,24 @@
+let count= 50;
+while (count < 0) {
+    console.log("Hola mundo");
+    count = count - 1;
+}   
+
+while (count < 0) {
+    console.log(count);
+    count = count - 1;
+}   ||
+
+// bucle: tarea que se repetira hasta que nosotros queramos
+
+// FOR:
+
+let names = ["Ryan", "Joe", "John"];
+
+console.log(names);
+
+// EJEMPLO:
+
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
