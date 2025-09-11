60e2811ab5ab50ffe661863d16b659b9f990a017
luisssarias <124051525@upq.edu.mx>
2025-09-11 11:34:15 -0600

    OPERANDO NUMS

---
+++ JSPrincipiantes/OperandoNum.js
@@ -0,0 +1,33 @@
+// OPERANDO NÚMEROS
+
+// 1)
+let numberOne = 60;  
+let numberTwo = 100;  
+let result = numberOne + numberTwo;  
+
+console.log("El resultado de la suma es: " + result);
+
+// 2)
+let name = "Joe";
+let lastName = "Carter";
+
+let completeName = name + " " + lastName;
+
+console.log("El nombre completo es: " + completeName);
+
+// 3)
+
+let numero1 = 100;  
+let numero2 = 500 ;  
+let resultado = numero1 > numero2;  
+
+console.log("El resultado de la comparación es: " + resultado);
+
+// 4)
+let passwordDB = "12345";
+let input = "3283294892";
+
+let resultado2 = passwordDB === input;
+
+console.log("El resultado de la comparación es: " + resultado2);    
+
