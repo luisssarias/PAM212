c5cfb2ccaf0ad84d5314bba27a07954f9c07b2f8
luisssarias <124051525@upq.edu.mx>
2025-09-11 13:16:16 -0600

    Funciones

---
+++ JSPrincipiantes/Funciones.js
@@ -0,0 +1,15 @@
+function greeting(){
+    console.log("Hola");
+}
+
+greeting();
+
+function add (n1, n2){
+    console.log(n1 + n2);
+}
+add(3, 2);
+add(100,300);
+
+add(3, 2);
+add(100,300);
+add(313131313,42424242);
